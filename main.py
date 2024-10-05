8896e64a5d6ba1ddd7a4521cd1b3959093e4bd8b
Vikash <vikashramesh2021@gmail.com>
2024-10-05 08:26:55 +0530

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import dpctl
+from sklearnex import patch_sklearn, config_context 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
